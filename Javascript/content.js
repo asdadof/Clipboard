da8b15353b1acb9a134202e1cc1f617dad0eba61
asdadof <sebnorlar@gmail.com>
2024-10-03 14:25:01 +0200

    CSS

--- Javascript/content.js
+++ Javascript/content.js
@@ -1,12 +1,23 @@
 document.addEventListener('copy', (e) => {
     const selectedText = window.getSelection().toString().trim();
     const copiedData = { text: selectedText };
-    
+
     e.preventDefault();
 
     if (selectedText) {
+
         e.clipboardData.setData('text/plain', selectedText);
+
+
+        try {
+            const url = new URL(selectedText); 
+            e.clipboardData.setData('text/uri-list', url.href); 
+        } catch (error) {
+        }
+
         copiedData.text = selectedText;
     }
-    chrome.runtime.sendMessage({action: "addToClipboard", text: selectedText});
+
+
+    chrome.runtime.sendMessage({ action: "addToClipboard", text: selectedText });
 });
